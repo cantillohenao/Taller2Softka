440bd330773025ab991ed556970b6883f1ec94e1
cantillohenao <cantillohenao@gmail.com>
2022-02-09 14:50:55 -0500

    creation of the different types of ships

---
+++ src/main/java/domain/Apolo.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class Apolo extends  MannedShips{
+    /**
+     * constructor initializing with the following parameters
+     *
+     * @param type     the type of the spaceships
+     * @param name     the name of the spaceships
+     * @param speed    the speed of the spaceships
+     * @param height   the speed of the spaceships
+     * @param weight   weight of the spaceships
+     * @param typeFuel typeFuel of the spaceships
+     */
+    public Apolo(String type, String name, long speed, float height, float weight, String typeFuel) {
+        super(type, name, speed, height, weight, typeFuel);
+    }
+}
